479d81921fef965818456b6c289bebd67552b64c
Nico <nicolai.proebstle@gmail.com>
2019-03-25 21:34:20 +0100

    v.2.0.
    Visual Update (Stories 1-3); JS Conditions

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -7,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent {
 
+  public message = '';
+  public output = 'Noch keine Chats vorhanden 😕';
+  public firstmessage = false;
+
+  //this.chatmessage = '';
+
   constructor() {
   }
-    send(chat_output, chat_input: string){
-      document.getElementById(chat_output).innerHTML = chat_input;
-      // alert(chat_input);
+    send(firstmessage, output, message){
+      if(message.length > 0){
+        if(firstmessage == false){
+        this.output = '';
+        document.getElementById('output').style.height = '440px';
+        document.getElementById('output').style.textAlign = 'right';
+        document.getElementById('output').style.paddingTop = '10px';
+        this.setflagfalse(firstmessage);
+      }
+        document.getElementById('chatInput').className = 'input';
+        this.output = message;
+        this.message = '';
+        return output;
+      } 
+      else {
+        document.getElementById('chatInput').className = 'alert';
+      }      
+    }
+
+    setflagfalse(firstmessage){
+      firstmessage = false;
+      return firstmessage;
     }
 }
