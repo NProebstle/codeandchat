13f8c1fcca0faa00811217f7b835d5d1764626e4
Nico <nicolai.proebstle@gmail.com>
2019-03-26 19:45:27 +0100

    v3.0.
    Neuer Style; Nicknames; Chat-Optimierung; Wartungsfähigkeit verbessert

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Message, CloneVisitor } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-chat',
@@ -7,35 +9,185 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent {
 
+  public alertText;
+  public colorsCollapsed = true;
+  public color = '#00802F';
+  public textcolor = '#00802F';
+  public initialOutput = 'Noch keine Chats vorhanden 😕';
   public message = '';
-  public output = 'Noch keine Chats vorhanden 😕';
-  public firstmessage = false;
-
-  //this.chatmessage = '';
+  public longMessage;
+  public messageCounter = 0;
+  public messageArray;
+  public nickName = '';
+  public output = '';
+  public prevMessageID = '';
 
   constructor() {
   }
-    send(firstmessage, output, message){
-      if(message.length > 0){
-        if(firstmessage == false){
-        this.output = '';
-        document.getElementById('output').style.height = '440px';
-        document.getElementById('output').style.textAlign = 'right';
-        document.getElementById('output').style.paddingTop = '10px';
-        this.setflagfalse(firstmessage);
+    send(){
+      if(this.nickCheck() && this.messageCheck()){
+        if(this.messageCounter == 0){
+        this.styleFirstChat();
+        }
+        if(!this.longMessage){
+          this.output = this.message;
+          this.message = '';
+          this.createMessage(this.output);
+          return this.prevMessageID;
+        }
+        else {
+          var numofMessages = this.messageArray.length;
+          var i;
+          for(i = 0; i < numofMessages; i++){
+            this.output = this.messageArray[i];
+            this.createMessage(this.output);
+          } 
+        }
       }
-        document.getElementById('chatInput').className = 'input';
-        this.output = message;
-        this.message = '';
-        return output;
-      } 
+    }      
+    
+
+    createMessage(output){
+      var c = this.messageCounter;
+      this.messageCounter = c+1;
+      var checkID = this.nickName + c;
+      if(c == 0){
+        this.createMessageElement(output);
+      }
+      else if(this.prevMessageID == checkID) {
+        this.mergeMessages(output);
+      }
       else {
-        document.getElementById('chatInput').className = 'alert';
-      }      
+        this.createMessageElement(output);
+      }
     }
 
-    setflagfalse(firstmessage){
-      firstmessage = false;
-      return firstmessage;
+    createMessageElement(output){
+      var currentdate = new Date();
+      if(this.color == 'white'){
+        this.textcolor = 'black';
+      } else {
+        this.textcolor = this.color;
+      }
+      var timestamp = currentdate.getHours() + ':' + currentdate.getMinutes();
+      var div = document.createElement("div");
+      div.id = this.nickName + this.messageCounter;
+      div.innerHTML = `<h4>${this.nickName}:</h4><p>${output}</p><span style="position:absolute;bottom: 10px;right:10px;padding-bottom: 5px;font-size: 14px;float: right;">${timestamp}</span>`;
+      div.style.cssText = `align-self: right;background-color: white;border: 1px solid ${this.color};border-radius: 5px;color: ${this.textcolor};clear: both;float: right;font-family: system-ui;font-size: 18px;line-height: 5px;margin: 10px 0;max-width: 80%;min-width: 300px;padding: 10px;padding-bottom: 5px;padding-top: 0px;position: relative;text-align: left;`;
+      div.className = 'message-right';
+      document.getElementById('output').appendChild(div);
+      this.prevMessageID = div.id;
+    }
+
+    mergeMessages(output){
+      var div = document.getElementById(this.prevMessageID);
+      div.innerHTML += `<p>${output}</p>`;
+      div.id = this.nickName + this.messageCounter;
+      this.prevMessageID = div.id;
+    }
+
+    messageCheck(){
+      if(!/^\s*$/.test(this.message)){
+        document.getElementById('chatInput').className = 'input';
+        if(this.message.length > 65){
+          this.messageArray = this.message.match(/(.{1,65})/g);
+          this.longMessage = true;
+        }
+        return true;
+      } 
+      else {
+        document.getElementById('alert').style.visibility = 'visible';
+        this.alertText += " Bitte geben Sie eine Nachricht ein!";
+        document.getElementById('chatInput').className = 'alert';
+        return false;
+      }
+    }
+
+    nickCheck(){
+      if(!/^\s*$/.test(this.nickName)){
+        if(this.nickName.length < 16){
+          document.getElementById('alert').style.visibility = 'hidden';
+          this.alertText = "";
+          document.getElementById('nickInput').className = 'nick';
+          return true;
+        }  
+        else {
+          document.getElementById('alert').style.visibility = 'visible';
+          this.alertText = "Der Nickname darf nicht länger als 15 Zeichen sein!";
+          this.messageCheck();
+          document.getElementById('nickInput').className = 'nickalert';
+          return false;
+        }
+      }
+      else {
+        document.getElementById('alert').style.visibility = 'visible';
+        this.alertText = "Nickname darf nicht leer sein!";
+        this.messageCheck();
+        document.getElementById('nickInput').className = 'nickalert';
+        return false;
+      }
+    }
+
+    changeColorDropdown(){
+      if(this.colorsCollapsed){
+        document.getElementById('cpGreen').className = 'cpList';
+        document.getElementById('cpBlue').className = 'cpList';
+        document.getElementById('cpRed').className = 'cpList';
+        document.getElementById('cpWhite').className = 'cpList';
+        document.getElementById('cpBlack').className = 'cpList';
+        document.getElementById('chatInput').style.width = '478px';
+        document.getElementById('chatInput').style.marginLeft = '0px';
+        document.getElementById('cpRed').style.background = 'red';
+        document.getElementById('cpRed').style.border = 'red';
+        document.getElementById('cpBlue').style.background = 'blue';
+        document.getElementById('cpBlue').style.border = 'blue';
+        document.getElementById('cpWhite').style.background = 'white';
+        document.getElementById('cpWhite').style.border = 'white';
+        document.getElementById('cpBlack').style.background = 'black';
+        document.getElementById('cpBlack').style.border = 'black';
+        document.getElementById('cpbtn').innerHTML = '◄';
+        this.colorsCollapsed = false;
+      }
+      else {
+        document.getElementById('cpGreen').className = 'cpListCollapsed';
+        document.getElementById('cpBlue').className = 'cpListCollapsed';
+        document.getElementById('cpRed').className = 'cpListCollapsed';
+        document.getElementById('cpWhite').className = 'cpListCollapsed';
+        document.getElementById('cpBlack').className = 'cpListCollapsed';
+        document.getElementById('cpbtn').innerHTML = '►';
+        document.getElementById('chatInput').style.width = '603px';
+        document.getElementById('chatInput').style.marginLeft = '-5px';
+        this.colorsCollapsed = true;
+      }
+    }
+
+    selectColor(color){
+      this.color = color;
+      document.getElementById('cpbtn').style.background = color;
+      document.getElementById('cpbtn').style.borderColor = color;
+      if(color == 'white'){
+        document.getElementById('cpbtn').style.color = 'black';
+        document.getElementById('cpbtn').style.borderColor = '#00802F';
+      }
+      else {
+        document.getElementById('cpbtn').style.color = 'white';
+      }
+      this.changeColorDropdown();
+      document.getElementById('alert').style.visibility = 'visible';
+      document.getElementById('alert').style.backgroundColor = 'rgba(0, 128, 47, 0.75)';
+      this.alertText = "Die Farbänderung wird erst mit einer Änderung des Nicknames wirksam!";
+      setTimeout(this.resetAlert, 8000);
+    }
+
+    styleFirstChat(){
+      this.initialOutput = '';
+      document.getElementById('output').style.height = '440px';
+      document.getElementById('output').style.textAlign = 'right';
+      document.getElementById('output').style.paddingTop = '10px';
+    }
+
+    resetAlert(){
+      document.getElementById('alert').style.backgroundColor = 'rgba(255, 80, 80, 0.75)';
+      document.getElementById('alert').style.visibility = 'hidden';
     }
 }
