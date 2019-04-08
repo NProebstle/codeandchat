96c1f07ca6eadbe81b5dd6da06163bd0942da189
Nico <nicolai.proebstle@gmail.com>
2019-04-08 15:47:46 +0200

    v3.1.
    - Timestamp ist jetzt immer aktuell
    - Bei Nachrichten mit "<" wird der Text nun korrekt angezeigt
    - Bei längeren Nachrichten (50+ Zeichen) wird nun beim Leerzeichen oder Komma getrennt und eine neue Zeile angezeigt
    - Zu Beginn und bei Änderung wird das aktuelle Datum angezeigt
    - Chat-Output scrollt nun automatisch zur letzten Nachricht
    - Nachrichten werden nun auch im Chat-Feld bei Eingabe von Shift+Enter abgeschickt
    - Kleinere Designanpassungen
    - Kleinere Anpassungen in der Code-Strultur

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Message, CloneVisitor } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-chat',
@@ -12,12 +10,16 @@ export class ChatComponent {
   public alertText;
   public colorsCollapsed = true;
   public color = '#00802F';
+  public dateBoxStyle = 'width:max-content;text-align:center;background:rgba(0, 128, 47, 0.8);border-radius:5px;font-size:18px;padding-top:5px;padding-bottom:5px;padding-left:10px;padding-right:10px;color:white;margin: 0 auto;';
   public textcolor = '#00802F';
   public initialOutput = 'Noch keine Chats vorhanden 😕';
   public message = '';
-  public longMessage;
+  public lastDate: string;
+  public lastTimestamp;
+  public longMessage = false;
   public messageCounter = 0;
   public messageArray;
+  public mergedMessages = 0;
   public nickName = '';
   public output = '';
   public prevMessageID = '';
@@ -32,7 +34,7 @@ export class ChatComponent {
         if(!this.longMessage){
           this.output = this.message;
           this.message = '';
-          this.createMessage(this.output);
+          this.createMessage();
           return this.prevMessageID;
         }
         else {
@@ -40,57 +42,117 @@ export class ChatComponent {
           var i;
           for(i = 0; i < numofMessages; i++){
             this.output = this.messageArray[i];
-            this.createMessage(this.output);
-          } 
+            this.createMessage();
+          }
+          this.message = '';
+          this.messageArray = null;
+          this.longMessage = false;
         }
       }
     }      
     
-
-    createMessage(output){
+    createMessage(){
       var c = this.messageCounter;
       this.messageCounter = c+1;
       var checkID = this.nickName + c;
-      if(c == 0){
-        this.createMessageElement(output);
-      }
-      else if(this.prevMessageID == checkID) {
-        this.mergeMessages(output);
-      }
-      else {
-        this.createMessageElement(output);
+      if(this.lastDate != this.currentDate()){
+        this.createDateBox();
+        this.createMessageElement();
+      }else {
+        if(c == 0){
+          this.createMessageElement();
+        }
+        else if(this.prevMessageID == checkID){
+         this.mergeMessages();
+        }
+        else {
+         this.createMessageElement();
+        }
       }
     }
 
-    createMessageElement(output){
-      var currentdate = new Date();
+    createMessageElement(){
       if(this.color == 'white'){
         this.textcolor = 'black';
       } else {
         this.textcolor = this.color;
       }
-      var timestamp = currentdate.getHours() + ':' + currentdate.getMinutes();
-      var div = document.createElement("div");
+      this.lastTimestamp = this.currentTimestamp();
+      var div = document.createElement('div');
       div.id = this.nickName + this.messageCounter;
-      div.innerHTML = `<h4>${this.nickName}:</h4><p>${output}</p><span style="position:absolute;bottom: 10px;right:10px;padding-bottom: 5px;font-size: 14px;float: right;">${timestamp}</span>`;
+
+      var header = document.createElement('h4');
+      header.innerText = `${this.nickName}:`;
+      div.appendChild(header);
+
+      var chat = document.createElement('p');
+      chat.innerText = `${this.output}`;
+      div.appendChild(chat);
+
+      var timestamp = document.createElement('span');
+      var spanID = this.nickName + this.messageCounter + 'span';
+      timestamp.id = spanID;
+      timestamp.style.cssText = "position:absolute;bottom: 10px;right:10px;padding-bottom: 5px;font-size: 14px;float: right;";
+      timestamp.innerText = `${this.lastTimestamp}`;
+      div.appendChild(timestamp);
+
       div.style.cssText = `align-self: right;background-color: white;border: 1px solid ${this.color};border-radius: 5px;color: ${this.textcolor};clear: both;float: right;font-family: system-ui;font-size: 18px;line-height: 5px;margin: 10px 0;max-width: 80%;min-width: 300px;padding: 10px;padding-bottom: 5px;padding-top: 0px;position: relative;text-align: left;`;
       div.className = 'message-right';
       document.getElementById('output').appendChild(div);
       this.prevMessageID = div.id;
+      this.mergedMessages = 0;
+      this.updateScroll();
     }
 
-    mergeMessages(output){
+    adaptTimestamp(){
+      if(this.currentTimestamp() != this.lastTimestamp){
+        var c = this.messageCounter - this.mergedMessages;
+        var spanID = this.nickName + c + 'span';
+        var span = document.getElementById(spanID);
+        var newTimestamp = this.currentTimestamp();
+        this.lastTimestamp = this.currentTimestamp();
+        span.innerHTML = newTimestamp;
+      }
+    }
+
+    currentDate(){
+      var date = new Date();
+      var day = date.getDay();
+      var month = date.getMonth();
+      var dayArray = new Array('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag');
+      var monthArray = new Array ('Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember');
+      var dmyTimestamp = dayArray[day] + ', ' + date.getDate() + '. ' + monthArray[month] + ' ' + date.getFullYear();
+      return dmyTimestamp;
+    }
+
+    currentTimestamp(){
+      var currentdate = new Date();
+      if(currentdate.getMinutes() < 10){
+        var timestamp = currentdate.getHours() + ':0' + currentdate.getMinutes();
+      }
+      else {
+        var timestamp = currentdate.getHours() + ':' + currentdate.getMinutes();
+      }
+      return timestamp;
+    }
+
+    mergeMessages(){
+      this.mergedMessages = this.mergedMessages + 1;
+      this.adaptTimestamp();
       var div = document.getElementById(this.prevMessageID);
-      div.innerHTML += `<p>${output}</p>`;
+      var newChat = document.createElement('p');
+      newChat.innerText = `${this.output}`;
+      div.appendChild(newChat);
       div.id = this.nickName + this.messageCounter;
       this.prevMessageID = div.id;
+      this.updateScroll();
     }
 
     messageCheck(){
       if(!/^\s*$/.test(this.message)){
         document.getElementById('chatInput').className = 'input';
         if(this.message.length > 65){
-          this.messageArray = this.message.match(/(.{1,65})/g);
+          this.messageArray = this.message.replace(/.{55}\S*\s+/g, "$&@").split(/\s+@/);
           this.longMessage = true;
         }
         return true;
@@ -161,6 +223,16 @@ export class ChatComponent {
       }
     }
 
+    createDateBox(){
+      var div = document.getElementById('output');
+      var dateBox = document.createElement('div');
+      var date = this.currentDate();
+      dateBox.innerText = date;
+      dateBox.style.cssText = this.dateBoxStyle;
+      div.appendChild(dateBox);
+      this.lastDate = date;
+    }
+
     selectColor(color){
       this.color = color;
       document.getElementById('cpbtn').style.background = color;
@@ -181,13 +253,20 @@ export class ChatComponent {
 
     styleFirstChat(){
       this.initialOutput = '';
-      document.getElementById('output').style.height = '440px';
-      document.getElementById('output').style.textAlign = 'right';
-      document.getElementById('output').style.paddingTop = '10px';
+      var div = document.getElementById('output');
+      div.style.height = '440px';
+      div.style.textAlign = 'right';
+      div.style.paddingTop = '10px';
+      this.createDateBox();
     }
 
     resetAlert(){
       document.getElementById('alert').style.backgroundColor = 'rgba(255, 80, 80, 0.75)';
       document.getElementById('alert').style.visibility = 'hidden';
+    }
+
+    updateScroll(){
+      var div = document.getElementById('output');
+      div.scrollTop = div.scrollHeight;
     }
 }
