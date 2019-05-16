ff82a8a561f44b5131f6454f366916d7e01c20b9
Nico <nicolai.proebstle@gmail.com>
2019-05-16 20:35:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,40 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = 3000;
 
+var chatHistory = [];
+var userHistory = [];
+var push = [];
+var init = false;
+var pushID = 0;
+
+   //init
+
+   //currentDate
+ if(!init){
+    var date = new Date();
+    var day = date.getDay();
+    var month = date.getMonth();
+    var dayArray = new Array('Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag');
+    var monthArray = new Array ('Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember');
+    var date = dayArray[day] + ', ' + date.getDate() + '. ' + monthArray[month] + ' ' + date.getFullYear();
+
+    //currentTimestamp
+    var currentdate = new Date();
+    if(currentdate.getMinutes() < 10){
+        var timestamp = currentdate.getHours() + ':0' + currentdate.getMinutes();
+    }
+    else {
+        var timestamp = currentdate.getHours() + ':' + currentdate.getMinutes();
+    }
+
+    var chatHistoryInit = [['Code&Chat 2019', 'EasyChat App v4', 'Initialized chatHistory', timestamp, date, '[//]'], ['[nickname]', '[message]', '[color]', '[timestamp]', '[date]', '[UID]']];
+    chatHistory = chatHistoryInit;
+
+    var userHistoryInit = [['Code&Chat 2019 – EasyChat App v4', 'Initialized userHistory', timestamp, date], ['[nickname]', '[color]', '[img]', '[UID]']];
+    userHistory = userHistoryInit;
+    init = true;
+}
+
 app.set('port', port);
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,47 +57,32 @@ app.use(function (req, res, next) {
     next();
 });
 
-// init
 app.get('/', function (req, res, next) {
     res.json({ message: 'Easy-Chat api works...' });
-    var timestamp = this.currentTimestamp();
-
-    //currentDate
-    var date = new Date();
-    var day = date.getDay();
-    var month = date.getMonth();
-    var dayArray = new Array('Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag');
-    var monthArray = new Array ('Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember');
-    var date = dayArray[day] + ', ' + date.getDate() + '. ' + monthArray[month] + ' ' + date.getFullYear();
-
-    //currentTimestamp
-    var currentdate = new Date();
-    if(currentdate.getMinutes() < 10){
-      var timestamp = currentdate.getHours() + ':0' + currentdate.getMinutes();
-    }
-    else {
-      var timestamp = currentdate.getHours() + ':' + currentdate.getMinutes();
-    }
-
-    var chatHistoryInit = [['Code&Chat 2019', 'EasyChat App v4', 'Initialized chatHistory', timestamp, date, '[//]'], ['[nickname]', '[message]', '[color]', '[timestamp]', '[date]', '[UID]']];
-    History.chatHistory = chatHistoryInit;
-
-    var userHistoryInit = [['Code&Chat 2019 – EasyChat App v4', 'Initialized userHistory', timestamp, date], ['[nickname]', '[color]', '[img]', '[UID]']];
-    Users.initUserHistory = userHistoryInit;
 });
 
-// history
+//send history
 app.get('/history', function (req, res, next) {
-    res.send(History.chatHistory);
+    console.log('[GET] chatHistory sent!');
+    res.send(chatHistory);
+});
+
+app.get('/profiles', function (req, res, next){
+    console.log('[GET] profiles sent!');
+    res.send(userHistory);
+});
+
+app.get('/push', function (req, res, next){
+    //console.log('[GET] push sent!')
+    res.send(push);
 });
 
 //receiveMessage
 app.post('/history', function (req, res, next) {
-    console.log(req.body);
-    var msgArray = req.body.msgArray;
-    var uid = msgArray[0];
-    var message = msgArray[1];
-    var userHistory = Users.userHistory;
+    console.log('[POST] received msg!');
+    var msgArray = req.body;
+    var uid = msgArray.msg[0];
+    var message = msgArray.msg[1];
 
     var userData = userHistory.filter(u => u[3] == uid);
     
@@ -89,38 +108,47 @@ app.post('/history', function (req, res, next) {
     }
 
     var chatArray = [nickname, message, color, timestamp, date, uid];
-    History.push = chatArray;
+    push = [chatArray, pushID];
+    pushID = pushID + 1;
     res.json({ message: 'Message added to history!' });
+    _push = chatArray;
+        if(_push[0] === Array){
+            var l = this._push.length;
+            for(var i = 0; i < l; i++){
+                chatHistory.push(_push[i]);
+            }
+        } else {
+            chatHistory.push(_push);
+        }
     return;
 });
 
 // receiveProfile
-app.post('/nicknames', function (req, res, next) {
-    console.log(req.body);
-    var profileArray = req.body.profileArray;
-    var nickname = profileArray[0];
-    var color = profileArray[1];
-    var img = profileArray[2];
-    var uid = profileArray[3];
+app.post('/profiles', function (req, res, next) {
+    console.log('[POST] received profile!');
+    var nickname = req.body.nickname;
+    var color = req.body.color;
+    var img = req.body.img;
+    var uid = req.body.uid;
 
     var userArray = [nickname, color, img, uid];
     
     var userExists = false;
-    for(var i = 0; i < Users.userHistory.length; i++){
-      var index = Users.userHistory[i][3].indexOf(uid);
+    for(var i = 0; i < userHistory.length; i++){
+      var index = userHistory[i][3].indexOf(uid);
       if(index > -1){
         index = i;
-        i = Users.userHistory.length;
+        i = userHistory.length;
         userExists = true;
       }
     }
     if(userExists){
-      Users.userHistory[index][0] = nickname;
-      Users.userHistory[index][1] = color;
-      Users.userHistory[index][2] = img;
+      userHistory[index][0] = nickname;
+      userHistory[index][1] = color;
+      userHistory[index][2] = img;
     return;
     } else {
-      Users.userHistory.push(userArray);
+      userHistory.push(userArray);
       return;
     }
 });
